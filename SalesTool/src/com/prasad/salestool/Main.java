e6f10e546510cca677a5161ce721bbabc78fb7dd
Thakur Prasad <thakur.prasad08@gmail.com>
2016-08-07 13:11:54 +0530

    SalesData and Main Created...

---
+++ SalesTool/src/com/prasad/salestool/Main.java
@@ -0,0 +1,21 @@
+package com.prasad.salestool;
+
+public class Main {
+	
+	public static void displayGreeting(){
+
+		System.out.println("GOOD MORNING");
+		System.out.println("GOOD Evening");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data =new SalesData();
+		data.display();
+		displayGreeting();
+		
+		
+
+	}
+
+}
---
+++ SalesTool/src/com/prasad/salestool/SalesData.java
@@ -0,0 +1,10 @@
+package com.prasad.salestool;
+
+public class SalesData {
+	
+	int data[]={0,1,2};
+
+	public void display(){
+		System.out.println("TODO : Implement data to dipslay");
+	}
+}
